986814121b436985c13098d00b1dc58ba97d6aa8
Brandon Piper <brandonpiper1@gmail.com>
2021-12-17 19:12:10 +0000

    Add project files.

---
+++ Doodler/Form1.Designer.cs
@@ -0,0 +1,117 @@
+﻿
+namespace Doodler
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.DrawPanel = new System.Windows.Forms.Panel();
+            this.colourPicker = new System.Windows.Forms.PictureBox();
+            this.close = new System.Windows.Forms.PictureBox();
+            this.Minimize = new System.Windows.Forms.PictureBox();
+            this.DrawPanel.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.colourPicker)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.close)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.Minimize)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // DrawPanel
+            // 
+            this.DrawPanel.BackColor = System.Drawing.Color.Maroon;
+            this.DrawPanel.Controls.Add(this.Minimize);
+            this.DrawPanel.Controls.Add(this.colourPicker);
+            this.DrawPanel.Controls.Add(this.close);
+            this.DrawPanel.Location = new System.Drawing.Point(0, 0);
+            this.DrawPanel.Margin = new System.Windows.Forms.Padding(0);
+            this.DrawPanel.Name = "DrawPanel";
+            this.DrawPanel.Size = new System.Drawing.Size(764, 1000);
+            this.DrawPanel.TabIndex = 0;
+            this.DrawPanel.MouseDown += new System.Windows.Forms.MouseEventHandler(this.StartDrawing);
+            this.DrawPanel.MouseMove += new System.Windows.Forms.MouseEventHandler(this.Draw);
+            this.DrawPanel.MouseUp += new System.Windows.Forms.MouseEventHandler(this.StopDrawing);
+            // 
+            // colourPicker
+            // 
+            this.colourPicker.BackColor = System.Drawing.Color.Black;
+            this.colourPicker.Location = new System.Drawing.Point(12, 42);
+            this.colourPicker.Name = "colourPicker";
+            this.colourPicker.Size = new System.Drawing.Size(24, 24);
+            this.colourPicker.TabIndex = 1;
+            this.colourPicker.TabStop = false;
+            this.colourPicker.Click += new System.EventHandler(this.ColourPicker);
+            // 
+            // close
+            // 
+            this.close.BackColor = System.Drawing.Color.Maroon;
+            this.close.Image = global::Doodler.Properties.Resources.Close;
+            this.close.Location = new System.Drawing.Point(12, 12);
+            this.close.Name = "close";
+            this.close.Size = new System.Drawing.Size(24, 24);
+            this.close.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.close.TabIndex = 0;
+            this.close.TabStop = false;
+            this.close.Click += new System.EventHandler(this.CloseGame);
+            // 
+            // Minimize
+            // 
+            this.Minimize.BackColor = System.Drawing.Color.Maroon;
+            this.Minimize.Image = global::Doodler.Properties.Resources.Minus;
+            this.Minimize.Location = new System.Drawing.Point(42, 12);
+            this.Minimize.Name = "Minimize";
+            this.Minimize.Size = new System.Drawing.Size(24, 24);
+            this.Minimize.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.Minimize.TabIndex = 3;
+            this.Minimize.TabStop = false;
+            this.Minimize.Click += new System.EventHandler(this.MinimiseButton);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.DarkSlateGray;
+            this.ClientSize = new System.Drawing.Size(826, 545);
+            this.Controls.Add(this.DrawPanel);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.DrawPanel.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.colourPicker)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.close)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.Minimize)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel DrawPanel;
+        private System.Windows.Forms.PictureBox close;
+        private System.Windows.Forms.PictureBox colourPicker;
+        private System.Windows.Forms.PictureBox Minimize;
+    }
+}
+
---
+++ Doodler/Form1.cs
@@ -0,0 +1,150 @@
+﻿using System;
+using System.Windows;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Doodler
+{
+    public partial class Form1 : Form
+    {
+        private List<List<Point>> lines;
+        private List<Color> lineColours;
+
+        private bool IsOpen = true;
+        private KeyHandler ghk;
+        private Graphics g;
+        private bool isPainting;
+        private Pen pen;
+        int lastX, lastY;
+        int loops;
+
+        public static Form1 Doodler
+        {
+            get
+            {
+                if (_doodler == null)
+                    _doodler = new Form1();
+                return _doodler;
+            }
+        }
+        private static Form1 _doodler;
+
+        public Form1()
+        {
+            InitializeComponent();
+            lines = new List<List<Point>>();
+            lineColours = new List<Color>();
+
+            _doodler = this;
+
+            Size = new Size((int)SystemParameters.VirtualScreenWidth, (int)SystemParameters.VirtualScreenHeight);
+            DrawPanel.Size = Size;
+            TransparencyKey = BackColor;
+            pen = new Pen(Color.FromArgb(200, Color.Black), 5);
+            DrawPanel.BackColor = Color.Transparent;
+            g = DrawPanel.CreateGraphics();
+            ghk = new KeyHandler(Keys.Scroll, this);
+            ghk.Register();
+        }
+
+        private void StartDrawing(object sender, MouseEventArgs e)
+        {
+            lines.Add(new List<Point>());
+            lineColours.Add(pen.Color);
+            lastX = e.X;
+            lastY = e.Y;
+            isPainting = true;
+        }
+
+        private void StopDrawing(object sender, MouseEventArgs e)
+        {
+            isPainting = false;
+        }
+
+        private void Draw(object sender, MouseEventArgs e)
+        {
+            if (!isPainting)
+                return;
+
+            loops++;
+
+            if (loops % 10 != 0)
+                return;
+
+            loops = 0;
+            g = DrawPanel.CreateGraphics();
+
+            g.DrawLine(pen, new Point(lastX, lastY), new Point(e.X, e.Y));
+            lines[lines.Count-1].Add(new Point(e.X, e.Y));
+
+            lastX = e.X;
+            lastY = e.Y;
+        }
+
+        private void HandleHotkey()
+        {
+            if (IsOpen)
+                Doodler.Hide();
+            else
+            {
+                Doodler.Show();
+                Redraw();
+            }
+
+            IsOpen = !IsOpen;
+        }
+
+        private void Redraw()
+        {
+            Color originalColour = pen.Color;
+            for (int i = 0; i < lines.Count; i++)
+            {
+                if (lines[i].Count <= 1)
+                    continue;
+
+                pen.Color = lineColours[i];
+                g.DrawLines(pen, lines[i].ToArray());
+            }
+            pen.Color = originalColour;
+        }
+
+        private void ColourPicker(object sender, EventArgs e)
+        {
+            ColorDialog c = new ColorDialog();
+            if (c.ShowDialog() == DialogResult.OK)
+            {
+                colourPicker.BackColor = c.Color;
+                pen.Color = c.Color;
+            }
+        }
+
+        private void CloseGame(object sender, EventArgs e)
+        {
+            Close();
+        }
+
+        private void MinimiseButton(object sender, EventArgs e)
+        {
+            HandleHotkey();
+        }
+
+        protected override void WndProc(ref Message m)
+        {
+            if (m.Msg == Constants.WM_HOTKEY_MSG_ID)
+                HandleHotkey();
+            base.WndProc(ref m);
+        }
+    }
+
+    public static class Constants
+    {
+        //windows message id for hotkey
+        public const int WM_HOTKEY_MSG_ID = 0x0312;
+    }
+}
